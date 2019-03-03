a8f9153681a2e9c76ad57960b2ac68dd87a06d51
Cristian Boga <bogacristianrudolf@gmail.com>
2019-03-03 18:16:48 +0200

    aded elements to the object

--- feature.js
+++ feature.js
@@ -1,1 +1,14 @@
-console.log(helper.objToArray({a: "Andrei"}))
+const result = helper.objToArray({
+    cristi: {
+        firstName: "Cristian",
+        lastName: "Boga",
+        age: 31
+    },
+    alex: {
+        firstName: "Alexandru",
+        lastName: "Okros",
+        age: 32
+    }
+});
+
+console.log(result);
